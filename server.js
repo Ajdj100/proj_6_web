c48e3e93f00cb6ae611607a932b71898c99d51ee
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2024-01-26 12:10:57 -0500

    Init branch. Add sql2, dotenv imports.

--- server.js
+++ server.js
@@ -1,32 +1,43 @@
+// Imports
 const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// Set up express app
 const app = express();
 const port = 8000;
+
+const connLimit = 100;
+
+const pool = mysql.createPool({
+    host : process.env.HOST,
+    user : process.env.USER,
+    password : process.env.PASSWORD,
+    database : process.env.DATABASE,
+    port : 3306,
+    connectionLimit : connLimit,
+    enableKeepAlive : true,
+    waitForConnections : true,
+    idleTimeout : 60000
+});
 
 // Server routes
 app.get("/", function (req, res) {
     res.send("Hello World!");
 });
 
-app.post("/", function (req, res) {
-    res.send("Hello World!");
+app.get("/testquery", function(req, res) {
+    pool.query('SELECT * FROM user;', function (error, results, fields) {
+        if (error) throw error;
+        for (let user in results) {
+            console.log(user.username);
+            console.log(user.password)
+        }
+    });
 });
 
-app.put("/", function (req, res) {
-    res.send("Hello World!");
+app.post("/login", function(req, res) {
 });
-
-app.patch("/", function (req, res) {
-    res.send("Hello World!");
-});
-
-app.delete("/", function (req, res) {
-    res.send("Hello World!");
-});
-
-app.options("/", function (req, res) {
-    res.send("Hello World!");
-});
-
 
 app.listen(port, function () {
     console.log(`Listening on port ${port}!`);
