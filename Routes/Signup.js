9ad503de30ebc6e9b5f94e25d480dd6911fe3b46
Amir Mansor <Amansor1893@conestogac.on.ca>
2024-01-27 13:39:21 -0500

    Current Signup route Endpoint, potentially done, checking final details

--- Routes/Signup.js
+++ Routes/Signup.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/', function (req, res, next) {  // handle specific get/post/etc methods
-    res.send('hello').status(200);
+router.post('/signup', function (req, res) {  // handle specific get/post/etc methods
+    pool.query(
+        "INSERT INTO user (username, password) VALUES (?, ?)",
+        [req.body.username, req.body.password],
+        (error, results) => {
+            if (error) {
+                res.status(500).send("Error Signing up a user");
+            } else {
+                res.status(200).json(results[0]);
+            }
+        }
+
+    );
 });
 
 module.exports = router;
