72f1eddf0189a5cda3e15c6848c6d26e46708ea0
Alistair Campbell <AJCampbell@conestogac.on.ca>
2024-01-28 02:15:29 -0500

    added /posts route

--- server.js
+++ server.js
@@ -70,20 +70,38 @@ app.post("/login", function(req, res) {
     );
 });
 
-app.get("/posts", function(req, res) {
-    pool.query(
-        'SELECT username, post_id, title, body FROM post INNER JOIN user ON post.user_id=user.user_id WHERE post_id < ? LIMIT ?;',
-        [req.body.lastID, req.body.limit], 
-        (error, results) => {
-            console.log(results);
-            if (error) {
-                res.status(500);
+app.get("/posts", function (req, res) {
+    if (req.query.current == -1) {
+        console.log('requested newest posts');
+        pool.query('SELECT username, post_id, title, body FROM post INNER JOIN user ON post.user_id = user.user_id ORDER BY post.post_id DESC LIMIT 5;',
+            [req.query.limit],
+            (error, results) => {
+                console.log(req.query.limit);
+                console.log(results);
+                if (error) {
+                    res.status(500);
+                }
+                else {
+                    res.status(200).json(results);
+                }
             }
-            else {
-                res.status(200).json(results);
+        );
+    } else {
+        console.log('requested more posts');
+        pool.query('SELECT username, post_id, title, body FROM post INNER JOIN user ON post.user_id = user.user_id WHERE post_id < ? ORDER BY post.post_id DESC LIMIT 5;',
+            [req.query.current, req.query.limit],
+            (error, results) => {
+                console.log(req.query.current, req.query.limit)
+                console.log(results);
+                if (error) {
+                    res.status(500);
+                }
+                else {
+                    res.status(200).json(results);
+                }
             }
-        }
-    );
+        );
+    }
 });
 
 app.listen(port, function () {
