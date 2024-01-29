8ad02453ad15880bb4e8d0bae06649ddcf4f7a1d
Cosmin Ene <nationalsown@gmail.com>
2024-01-28 23:11:45 -0500

    Update server.js
    added back the const cookieparser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 // Set up express app
